f31dfe11813ce79fd2b25d4774c73f4d0c9041f7
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2025-06-08 02:21:27 +0700

    fix: order of table drop (for preventation), ApplicationDetail should be dropped before ApplicantProfile

--- src/Database/schema.sql
+++ src/Database/schema.sql
@@ -1,6 +1,8 @@
 -- src/Database/schema.sql
-DROP TABLE IF EXISTS ApplicantProfile;
+
+-- ApplicationDetail should be deleted before ApplicantProfile (Child class, Basis Data moment)
 DROP TABLE IF EXISTS ApplicationDetail;
+DROP TABLE IF EXISTS ApplicantProfile;
 
 -- ApplicantProfile
 CREATE TABLE ApplicantProfile (
@@ -13,18 +15,18 @@ CREATE TABLE ApplicantProfile (
 
     -- PK = applicant_id
     PRIMARY KEY (applicant_id)
-)
+);
 
 -- ApplicationDetail
 CREATE TABLE ApplicationDetail (
     detail_id INT NOT NULL,
     applicant_id INT NOT NULL,
     application_role VARCHAR(100) DEFAULT NULL,
-    vc_path TEXT;
+    cv_path TEXT;
 
     -- FK = applicant_id references ApplicantProfile(applicant_id)
     FOREIGN KEY (applicant_id) REFERENCES ApplicantProfile(applicant_id),
 
     -- PK = detail_id
     PRIMARY KEY (detail_id)
-)
+);
